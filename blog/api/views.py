11e83936ef822b51681675694e7f71e11bad12bf
ShipaShipovnik <shipovnik2020d@gmail.com>
2024-09-08 19:30:09 +0300

    Комментарии и срилизатор коммнтариев

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,31 +1,52 @@
+from symtable import Class
+
+from django.core.serializers import serialize
 from django.shortcuts import render
-from rest_framework import generics,permissions
+from rest_framework import generics, permissions
 from . import serializers
 from .permissions import *
 from django.contrib.auth.models import User
-from .models import Post
+from .models import Post, Comment
 
-#USERS=======================
+
+# USERS=======================
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
-    serializer_class = serializers.UserSerializers
+    serializer_class = serializers.UserSerializer
+
 
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
-    serializer_class = serializers.UserSerializers
+    serializer_class = serializers.UserSerializer
 
-#POST=======================
+
+# POST=======================
 class PostList(generics.ListAPIView):
     queryset = Post.objects.all()
     serializer_class = serializers.PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-    def perform_create(self,serializer):
+    def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = serializers.PostSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsOwnerReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerReadOnly]
 
 
+# COMMENT=======================
+class CommentList(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerReadOnly]
